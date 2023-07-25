d52ebaf7b0c0c120285b243f5dae6998064f6e40
UX400-A13 UX400-A13 <P001529>
2023-07-25 12:44:40 +0000

    switched to components,still errors for OData

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,14 +1,15 @@
-sap.ui.define([
-    "sap/ui/core/UIComponent"
-],
-    function (UIComponent) {
-        "use strict";
+sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+  "use strict";
 
-        return UIComponent.extend("sap.training.exc.Component", {
+  return UIComponent.extend("sap.training.exc.Component", {
+    // must have!
+    metadata: {
+      manifest: "json",
+      interfaces: ["sap.ui.core.IAsyncContentCreation"],
+    },
 
-
-
-
-        });
-    }
-);
+    init: function () {
+      UIComponent.prototype.init.apply(this, arguments);
+    },
+  });
+});
--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,16 @@
-sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/ComponentContainer"],
+  function (ComponentContainer) {
+    "use strict";
 
-  XMLView.create({
-    id: "App",
-    viewName: "sap.training.exc.view.App",
-  }).then(function (oView) {
-    oView.placeAt("content");
-  });
-});
+    new ComponentContainer({
+      id: "container",
+      name: "sap.training.exc",
+      manifest: true,
+      async: true,
+      settings: {
+        id: "sap.training.exc",
+      },
+    }).placeAt("content");
+  }
+);
